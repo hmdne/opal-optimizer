c215be954acdf45ac00df8ba74d77d7c602415cd
hmdne <54514036+hmdne@users.noreply.github.com>
2021-12-23 17:41:47 +0100

    Release v0.1.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.1.6
+
+* Further Opal 1.4 compatibility
+
 ## v0.1.5
 
 * Opal 1.4 compatibility
--- lib/opal/optimizer/version.rb
+++ lib/opal/optimizer/version.rb
@@ -1,5 +1,5 @@
 module Opal
   class Optimizer
-    VERSION = "0.1.5"
+    VERSION = "0.1.6"
   end
 end
